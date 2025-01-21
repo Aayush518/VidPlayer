8d155b976de2d0380ec05d3c91a069e8cdb5a3b0
adhikareeprayush <adhikareeprayush@gmail.com>
2025-01-21 15:26:37 +0545

    video player

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Video from "./components/Video";
+
+const App = () => {
+  return (
+    <div>
+      <Video />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Video.tsx
@@ -0,0 +1,174 @@
+import React from "react";
+import ReactPlayer from "react-player/lazy";
+
+const videos = [
+  {
+    id: "1",
+    title: "Big Buck Bunny",
+    thumbnailUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/Big_Buck_Bunny_thumbnail_vlc.png/1200px-Big_Buck_Bunny_thumbnail_vlc.png",
+    duration: "8:18",
+    uploadTime: "May 9, 2011",
+    views: "24,969,123",
+    author: "Vlc Media Player",
+    videoUrl:
+      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+    description:
+      "Big Buck Bunny tells the story of a giant rabbit with a heart bigger than himself. When one sunny day three rodents rudely harass him, something snaps... and the rabbit ain't no bunny anymore! In the typical cartoon tradition he prepares the nasty rodents a comical revenge.\n\nLicensed under the Creative Commons Attribution license\nhttp://www.bigbuckbunny.org",
+    subscriber: "25254545 Subscribers",
+    isLive: true,
+  },
+  {
+    id: "2",
+    title: "The first Blender Open Movie from 2006",
+    thumbnailUrl: "https://i.ytimg.com/vi_webp/gWw23EYM9VM/maxresdefault.webp",
+    duration: "12:18",
+    uploadTime: "May 9, 2011",
+    views: "24,969,123",
+    author: "Blender Inc.",
+    videoUrl:
+      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",
+    description:
+      "Song : Raja Raja Kareja Mein Samaja\nAlbum : Raja Kareja Mein Samaja\nArtist : Radhe Shyam Rasia\nSinger : Radhe Shyam Rasia\nMusic Director : Sohan Lal, Dinesh Kumar\nLyricist : Vinay Bihari, Shailesh Sagar, Parmeshwar Premi\nMusic Label : T-Series",
+    subscriber: "25254545 Subscribers",
+    isLive: true,
+  },
+  {
+    id: "3",
+    title: "For Bigger Blazes",
+    thumbnailUrl: "https://i.ytimg.com/vi/Dr9C2oswZfA/maxresdefault.jpg",
+    duration: "8:18",
+    uploadTime: "May 9, 2011",
+    views: "24,969,123",
+    author: "T-Series Regional",
+    videoUrl:
+      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4",
+    description:
+      "Song : Raja Raja Kareja Mein Samaja\nAlbum : Raja Kareja Mein Samaja\nArtist : Radhe Shyam Rasia\nSinger : Radhe Shyam Rasia\nMusic Director : Sohan Lal, Dinesh Kumar\nLyricist : Vinay Bihari, Shailesh Sagar, Parmeshwar Premi\nMusic Label : T-Series",
+    subscriber: "25254545 Subscribers",
+    isLive: true,
+  },
+  {
+    id: "4",
+    title: "For Bigger Escape",
+    thumbnailUrl:
+      "https://img.jakpost.net/c/2019/09/03/2019_09_03_78912_1567484272._large.jpg",
+    duration: "8:18",
+    uploadTime: "May 9, 2011",
+    views: "24,969,123",
+    author: "T-Series Regional",
+    videoUrl:
+      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4",
+    description:
+      " Introducing Chromecast. The easiest way to enjoy online video and music on your TV—for when Batman's escapes aren't quite big enough. For $35. Learn how to use Chromecast with Google Play Movies and more at google.com/chromecast.",
+    subscriber: "25254545 Subscribers",
+    isLive: false,
+  },
+  {
+    id: "5",
+    title: "Big Buck Bunny",
+    thumbnailUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/Big_Buck_Bunny_thumbnail_vlc.png/1200px-Big_Buck_Bunny_thumbnail_vlc.png",
+    duration: "8:18",
+    uploadTime: "May 9, 2011",
+    views: "24,969,123",
+    author: "Vlc Media Player",
+    videoUrl:
+      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+    description:
+      "Big Buck Bunny tells the story of a giant rabbit with a heart bigger than himself. When one sunny day three rodents rudely harass him, something snaps... and the rabbit ain't no bunny anymore! In the typical cartoon tradition he prepares the nasty rodents a comical revenge.\n\nLicensed under the Creative Commons Attribution license\nhttp://www.bigbuckbunny.org",
+    subscriber: "25254545 Subscribers",
+    isLive: true,
+  },
+  {
+    id: "6",
+    title: "For Bigger Blazes",
+    thumbnailUrl: "https://i.ytimg.com/vi/Dr9C2oswZfA/maxresdefault.jpg",
+    duration: "8:18",
+    uploadTime: "May 9, 2011",
+    views: "24,969,123",
+    author: "T-Series Regional",
+    videoUrl:
+      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4",
+    description:
+      "Song : Raja Raja Kareja Mein Samaja\nAlbum : Raja Kareja Mein Samaja\nArtist : Radhe Shyam Rasia\nSinger : Radhe Shyam Rasia\nMusic Director : Sohan Lal, Dinesh Kumar\nLyricist : Vinay Bihari, Shailesh Sagar, Parmeshwar Premi\nMusic Label : T-Series",
+    subscriber: "25254545 Subscribers",
+    isLive: false,
+  },
+  {
+    id: "7",
+    title: "For Bigger Escape",
+    thumbnailUrl:
+      "https://img.jakpost.net/c/2019/09/03/2019_09_03_78912_1567484272._large.jpg",
+    duration: "8:18",
+    uploadTime: "May 9, 2011",
+    views: "24,969,123",
+    author: "T-Series Regional",
+    videoUrl:
+      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4",
+    description:
+      " Introducing Chromecast. The easiest way to enjoy online video and music on your TV—for when Batman's escapes aren't quite big enough. For $35. Learn how to use Chromecast with Google Play Movies and more at google.com/chromecast.",
+    subscriber: "25254545 Subscribers",
+    isLive: true,
+  },
+  {
+    id: "8",
+    title: "The first Blender Open Movie from 2006",
+    thumbnailUrl: "https://i.ytimg.com/vi_webp/gWw23EYM9VM/maxresdefault.webp",
+    duration: "12:18",
+    uploadTime: "May 9, 2011",
+    views: "24,969,123",
+    author: "Blender Inc.",
+    videoUrl:
+      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",
+    description:
+      "Song : Raja Raja Kareja Mein Samaja\nAlbum : Raja Kareja Mein Samaja\nArtist : Radhe Shyam Rasia\nSinger : Radhe Shyam Rasia\nMusic Director : Sohan Lal, Dinesh Kumar\nLyricist : Vinay Bihari, Shailesh Sagar, Parmeshwar Premi\nMusic Label : T-Series",
+    subscriber: "25254545 Subscribers",
+    isLive: false,
+  },
+];
+
+const Video = () => {
+  const [currentVideo, setCurrentVideo] = React.useState(videos[0]);
+
+  return (
+    <div className="h-screen grid grid-cols-4 w-full">
+      {/* Video Section */}
+      <div className="col-span-3 flex flex-col items-center justify-center">
+        <ReactPlayer
+          url={currentVideo.videoUrl}
+          controls
+          playing
+          width="90%"
+          height="60%"
+        />
+      </div>
+
+      {/* Playlist Section */}
+      <div className="col-span-1 bg-gray-100 overflow-y-auto">
+        <div className="flex flex-col space-y-4 p-4">
+          {videos.map((video) => (
+            <div
+              key={video.id}
+              className="flex space-x-2 p-2 border-b border-gray-300"
+              onClick={() => setCurrentVideo(video)}
+            >
+              <img
+                src={video.thumbnailUrl}
+                alt={video.title}
+                className="w-24 h-16 object-cover"
+              />
+              <div className="flex flex-col justify-between">
+                <h1 className="text-sm font-bold">{video.title}</h1>
+                <p className="text-xs text-gray-600">{video.author}</p>
+                <p className="text-xs text-gray-600">{video.views} views</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Video;
